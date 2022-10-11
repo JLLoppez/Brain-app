28d800c4e6502c4fd5219ac7ffcbf7f452c77483
JLLoppez <99202989+JLLoppez@users.noreply.github.com>
2022-10-11 14:15:34 +0200

    Update Time.js

--- src/components/Countdown/Time.js
+++ src/components/Countdown/Time.js
@@ -42,7 +42,7 @@ class CountDown extends Component {
     }
   }
   componentDidMount() {
-    this.deadline = new Date("Oct 10, 2022 23:59:00").getTime();
+    this.deadline = new Date("Dec 31, 2022 23:59:00").getTime();
 
     this.x = setInterval(this.count, 1000);
   }
